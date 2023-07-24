37b0e87bc71a20286cf314fcadddf38380424c34
bERZZANI INC <66145809+berzzani@users.noreply.github.com>
2023-07-24 17:24:44 -0600

    Initial Commit

---
+++ CollectionViewTest/CollectionViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewTestApp.swift
+//  CollectionViewTest
+//
+//  Created by Dave Durazzani on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CollectionViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
